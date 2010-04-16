fd7f9ccf6a7afdf5627b02cd233877e292f8dc63
Vincent Montfort <>
2010-04-16 12:48:19 +0000

    Bug fix for inlining: A handler catching the old subroutine code was not removed and therefore catch invalid part of code ... Add comments on code. Modify calculation of new handler because scope was not good (david could you verify if your ok ?)

--- src/jsrInline.ml
+++ src/jsrInline.ml
@@ -41,6 +41,9 @@ let array_fold f t b =
     aux 0
 
 (* all beginning of subroutines (astore ...) *)
+(** [get_jsr code] returns a couple composed of a set containing the
+    target pp of Jsr instructions and a associative list that associate
+    the pp of the Jsr instruction and its target.*)
 let get_jsr code =
   array_fold 
     (fun i op (jsr,l) ->
@@ -51,18 +54,29 @@ let get_jsr code =
     (Ptset.empty, [])
 
 (* for each pc, all predecessor (except pc-1) *)
+(** [compute_jump_target code] returns an array of Pset containing all
+    jump sources for a jump target (Handlers, Goto, If, ... except Jsr and Ret
+    jump...). Index of array is the pp target of jump and Pset
+    contained represents all possibles pp sources for the jump to pp
+    target.*)
 let compute_jump_target code =
   let jump_target = Array.make (Array.length code.c_code) Ptset.empty in
   let add i j = jump_target.(j) <- Ptset.add i jump_target.(j) in
+    (* add numbers from i to j-1 to set if i<j*)
   let rec add_interval i j set =
     if i>=j then set
     else 
       if code.c_code.(i)<>OpInvalid
       then add_interval (i+1) j (Ptset.add i set) 
-      else add_interval (i+1) j set in    
+      else add_interval (i+1) j set 
+  in    
+    (* add all pp covered by the handler e in jump_target for start pp of the handler e*)
   let add_handler e =
-    jump_target.(e.e_handler) <- add_interval e.e_start e.e_end jump_target.(e.e_handler) in
+    jump_target.(e.e_handler) <- add_interval e.e_start e.e_end jump_target.(e.e_handler) 
+  in
+    (* execute add handler for all code handlers (contained in exception table)*)
     List.iter add_handler code.c_exc_tbl;
+    (* add all sources of jump for the targets of jump in jump_target*)
     Array.iteri
       (fun i instr ->
 	 match instr with
@@ -181,7 +195,7 @@ let next_pc c i =
       !k
   with _ -> i
 
-(* return the set of prog points reachable from [i] and the set of OpRet at exit points *)
+(** [explore_from code i] returns the couple composed by the set of OpRet pp at exit points and the set of prog points reachable from [i] in code [code].*)
 let explore_from code i =
   match code.c_code.(i) with
     | OpStore (`Object,x) ->
@@ -212,14 +226,19 @@ let explore_from code i =
     if not (Ptset.cardinal exxit = 1) then raise InlineSubroutineFailed;
     (Ptset.choose exxit,set)
 
-(* try to build an interval from the subroutine prog points *)
+(** [make_intervals_union code set] try to build an interval from the
+    subroutine pp contained in [set].  @Raise InlineSubroutineFailed if
+    more than one interval is needed to include all pp of [set]*)
 let make_intervals_union code set =
+  (* if i+1 is a pp of code, then return first not OpInvalid from i+1. if not return i+1*)
   let rec next i = 
     if (i+1<Array.length code.c_code) then
       if code.c_code.(i+1) = OpInvalid 
       then next (i+1)
       else i+1 
     else i+1 in
+    (* returns the last contigous pp included in
+       [set] from [i] *)
   let rec fill set i = 
     let next = next i in
       if Ptset.mem next set
@@ -230,12 +249,13 @@ let make_intervals_union code set =
     else
       let i = Ptset.min_elt set in
       let (j,set) = fill (Ptset.remove i set) i in
-	next_int set ((i,j)::l) in
+	next_int set ((i,j)::l) 
+  in
   let intervals = next_int set [] in
     match intervals with
       | [x] -> x
       | _ -> raise InlineSubroutineFailed
-    
+	  
 
 let rec print_list_sep_rec sep pp = function
   | [] -> ""
@@ -244,7 +264,7 @@ let rec print_list_sep_rec sep pp = function
 let print_list_sep sep pp = function
   | [] -> ""
   | x::q -> (pp x)^(print_list_sep_rec sep pp q)
-	
+      
 let print_set s =
   "{"^(print_list_sep "," string_of_int (Ptset.elements s))^"}"
 
@@ -254,8 +274,10 @@ let inter (a,b) (c,d) =
   let low = max a c in
   let high = min b d in
     if low <= high then Some (low,high) else None
-      
+
+
 let inline code instrs subroutines =
+  (* Should not be size+pps_sub.size(+1 for ret ?) instead ?*)
   let new_size =
     List.fold_left
       (fun size (_,start) -> 
@@ -266,89 +288,122 @@ let inline code instrs subroutines =
     Array.blit code.c_code 0 new_code 0 (Array.length code.c_code);
     let current = ref (Array.length code.c_code) in
     let handlers = ref [] in
-    List.iter
-      (fun (i,start) ->
-	 let (code_start,codes,code_exit,size,handlers_mine,handlers_global) = subroutines start in
-	   Ptset.iter (fun i -> new_code.(i) <- OpInvalid) codes;
-	   new_code.(code_exit) <- OpInvalid;
-	   Ptset.iter
-	     (fun i -> 
-		if i <> code_start then
-		  new_code.(!current+i-code_start) <- code.c_code.(i)
-	     ) codes;
-	   new_code.(i) <- OpGoto ((next_after_jsr code code_start)+ !current-code_start-i);
-	   new_code.(!current+code_exit-code_start) <- 
-	     OpGoto ((next_after_jsr code i)-(!current+code_exit-code_start));
-	   handlers := 
-	     (List.map (fun e -> 
-			  {
-			    e_start = e.e_start - code_start + !current;
-			    e_end = e.e_end - code_start + !current;
-			    e_handler = e.e_handler - code_start + !current;
-			    e_catch_type = e.e_catch_type
-			  }) handlers_mine)
-	   @ (List.map (fun e -> 
-			  let (e_start,e_end) = 
-			    match inter (code_start,code_start+size) (e.e_start,e.e_end) with
-			      | None -> assert false
-			      | Some x -> x in
-			  {
-			    e_start = e_start - code_start + !current;
-			    e_end = e_end - code_start + !current;
-			    e_handler = e.e_handler;
-			    e_catch_type = e.e_catch_type
-			  }) handlers_global)
-	   @ !handlers;
-	   current  := !current + size
-      ) instrs;
+      (* Create copy of subroutine code for each Jsr instr src_pp and
+	 necessary handlers linked to subroutines ...*)
+      List.iter
+	(fun (i,start) ->
+	   let (code_start,codes,code_exit,
+		size,handlers_mine,handlers_global) = subroutines start in
+	     Ptset.iter (fun i -> new_code.(i) <- OpInvalid) codes;
+	     new_code.(code_exit) <- OpInvalid;
+	     (* copy all suroutine code at the end of new code*)
+	     Ptset.iter
+	       (fun i -> 
+		  if i <> code_start then
+		    new_code.(!current+i-code_start) <- code.c_code.(i)
+	       ) codes;
+	     (* Replace Jsr instr by Goto copy of jsr code for [i] src_pp*)
+	     new_code.(i) <- 
+	       OpGoto 
+	       ((next_after_jsr code code_start)+ !current-code_start-i);
+	     (* Replace Ret instr by Goto [i] src_pp*)
+	     new_code.(!current+code_exit-code_start) <- 
+	       OpGoto 
+	       ((next_after_jsr code i)-(!current+code_exit-code_start));
+	     handlers := 
+	       (List.map (fun e -> 
+			    {
+			      e_start = e.e_start - code_start + !current;
+			      e_end = e.e_end - code_start + !current;
+			      e_handler = e.e_handler - code_start + !current;
+			      e_catch_type = e.e_catch_type
+			    }) handlers_mine)
+	     @ (List.map (fun e -> 
+			    let (e_start,e_end) = 
+			      match inter (code_start,code_start+size) (e.e_start,e.e_end) with
+				| None -> assert false
+				| Some x -> x in
+			      {
+				e_start = e_start - code_start + !current;
+				e_end = e_end - code_start + !current;
+				e_handler = e.e_handler;
+				e_catch_type = e.e_catch_type
+			      }
+			 ) handlers_global)
+	     @ !handlers;
+	     current  := !current + size
+	) 
+	instrs;
+      
       let next = next_pc new_code in
-    {
-      c_max_stack = code.c_max_stack;
-      c_max_locals = code.c_max_locals;
-      c_code = new_code;
-      c_exc_tbl = 
-	(List.filter (fun e -> new_code.(e.e_handler)<>OpInvalid) code.c_exc_tbl)
-	  @(List.map
-	      (fun e -> 
-		 {
-		   e_start = next e.e_start;
-		   e_end = next e.e_end;
-		   e_handler = e.e_handler;
-		   e_catch_type = e.e_catch_type
-		 }) !handlers);
-      c_line_number_table = None;
-      c_local_variable_table = None;
-      c_stack_map_midp = None;
-      c_stack_map_java6 =  None;
-      c_attributes = code.c_attributes
-    }
+      let first_valid pp = 
+	if new_code.(pp)=OpInvalid
+	then next pp
+	else pp
+      in
+	{
+	  c_max_stack = code.c_max_stack;
+	  c_max_locals = code.c_max_locals;
+	  c_code = new_code;
+	  (* Remove not valid anymore handlers (those included in
+	     subroutine code and those who catch old subroutine code. Add
+	     the new handlers created.*)
+	  c_exc_tbl = 
+	    (List.filter 
+	       (fun e -> 
+		  (new_code.(e.e_handler)<>OpInvalid)&&
+		    (new_code.(e.e_start)<>OpInvalid)  
+	       ) 
+	       code.c_exc_tbl)
+	    @(List.map
+		(fun e -> 
+		   (* TODO: ask david if he is ok with this fix !*)
+		   {
+		     e_start = first_valid e.e_start;
+		     e_end = first_valid e.e_end;
+		     e_handler = e.e_handler;
+		     e_catch_type = e.e_catch_type
+		   }) !handlers);
+	  c_line_number_table = None;
+	  c_local_variable_table = None;
+	  c_stack_map_midp = None;
+	  c_stack_map_java6 =  None;
+	  c_attributes = code.c_attributes
+	}
 
+(** [scan_subroutine code jump_target start] returns (start pp, pp included, pp of Ret instr, length of subroutine, handlers (that catch sub) which code is included in subroutines, ohter handlers (that catch sub)).
+    @Raise InlineSubroutineFailed if all pp of sub are not contiguous.*)
 let scan_subroutine code jump_target start =
   (* start must be only reachable from the OpJsr *)
   if not (no_direct_succ (pred code start)) then raise InlineSubroutineFailed;
   if not (Ptset.is_empty jump_target.(start)) then raise InlineSubroutineFailed;
-  
+  (* (pp of ret instruction, pp of instructions reachable
+     that could lead to a ret instruction)*)
   let (ret,succs) = explore_from code start in
+    (* returns an interval if all instructions in subroutines
+       are contiguous instructions, raise exception if not. *)
   let (start,endd) = make_intervals_union code succs in
-
     Ptset.iter
       (fun j ->
 	 (* no jumps in the subroutine from outside *)
 	 if not (Ptset.subset jump_target.(j) succs) then raise InlineSubroutineFailed)
       succs;
 
-    let handlers = (* all handlers that catch the subroutine *)
+    let handlers = (* all handlers that catch the subroutine instructions*)
       List.filter
 	(fun e ->  
 	   Ptset.exists
 	     (fun i -> e.e_start <= i && i < e.e_end)
 	     succs) 
 	code.c_exc_tbl in
+      (* handlers which code is included in subrountine, others *)
     let (handlers_mine,handlers_global) = 
       List.partition
 	(fun e -> Ptset.mem e.e_handler succs || e.e_handler = ret)
-	handlers in
+	handlers 
+    in
       (start,succs,ret,(max endd (ret+1))-start,handlers_mine,handlers_global)
+
 
 let nb_meth = ref 0
 
@@ -388,15 +443,20 @@ let test target = Javalib.iter
   Printf.printf "%d methods with subroutines\n" !nb_meth
 
 let inline code = 
+  (* couple (Pset (Jsr targets), List (jsr_src_pp,jsr_trg_pp))*)
   let (jsr,instrs) = get_jsr code in
     if Ptset.is_empty jsr then Some code
     else
       try
+	(* for all jumps (handlers, goto, ... except jsr) Array(trg_pp, Pset(src_pp))*)
 	let jump_target = compute_jump_target code in
-	let subroutines = Ptset.fold
-			    (fun i map ->
-			       Ptmap.add i (scan_subroutine code jump_target i) map)
-			    jsr Ptmap.empty in
+	let subroutines = 
+	  Ptset.fold
+	    (fun i map ->
+	      Ptmap.add 
+		 i (scan_subroutine code jump_target i) map)
+	    jsr Ptmap.empty 
+	in
 	Some (inline code instrs (fun i -> Ptmap.find i subroutines))
       with
 	  InlineSubroutineFailed -> None
