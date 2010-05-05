78895ddc4125a18a2ddf23b24d544c341d7de8b2
Vincent Montfort <>
2010-05-05 12:03:55 +0000

    add documentation on reachMeth

--- src/reachableMethods.mli
+++ src/reachableMethods.mli
@@ -1,5 +1,16 @@
+(** ReachabeMethods allows to compute the methods of a program that
+    are reachable from a set of entry points.  This can be used in
+    order to update the field [parsed_method] of {!JProgram.program}.
+    The implementation is not very efficient, but it is simple and
+    rely on the {!Safe} framework. *)
+
 open Javalib_pack
 
+
+(** [compute_reachable_methods p entry_points] will computes the methods that
+    are reachable from the entry points [entry_points] in the program [p].  It
+    assumes that [p.parsed_methods] is a correct over-approximation of reachable
+    methods. *)
 val compute_reachable_methods :
     JCode.jcode JProgram.program -> 
     JBasics.class_method_signature list -> 
