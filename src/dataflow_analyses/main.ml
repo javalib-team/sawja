cd71aceeaeee0aa723424f15fbdfd1f1f6446aa8
Vincent Montfort <>
2010-10-26 16:13:03 +0000

    add an example of available expression analysis (flow insensitive version) on SSA form

--- src/dataflow_analyses/main.ml
+++ src/dataflow_analyses/main.ml
@@ -6,6 +6,7 @@ open JCode
 open Javalib
 
 let run filename =
+  print_endline "JBir:";
   iter
     (fun i_or_c -> 
        ignore
@@ -30,6 +31,29 @@ let run filename =
 		   ir_code.JBir.code;
 		 print_newline ())
 	    (map_interface_or_class_context JBir.transform i_or_c)))
+    filename;
+  print_endline "JBirSSA:";
+  iter
+    (fun i_or_c -> 
+       ignore
+	 (map_interface_or_class_context ~force:true
+	    (fun cm ir_code ->
+	       Printf.printf "class %s: method %s\n"
+		 (JPrint.class_name (get_name i_or_c))
+		 (JPrint.method_signature cm.cm_signature);
+	       let ae = AvailableExprSSA.run ir_code in
+		 List.iter print_endline (JBirSSA.print ~phi_simpl:false ir_code);
+		 print_newline ();
+		 Array.iteri 
+		   (fun i v -> 
+		      Printf.printf "--> AE[%s(%d)]: %s\n"
+			(JBirSSA.var_name_g v)
+			(JBirSSA.index v)
+			(AvailableExprSSA.to_string (ae i));)
+		   ir_code.JBirSSA.vars;
+		 print_newline ();
+	    )
+	    (map_interface_or_class_context JBirSSA.transform i_or_c)))
     filename
 
 let _ =
