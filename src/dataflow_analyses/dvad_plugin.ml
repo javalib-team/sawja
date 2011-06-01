2999e1a6eca43e614ac2fa7e665dcf3bfab195b0
Vincent Montfort <>
2011-06-01 15:22:12 +0000

    forgot to add live_bir.mli file

--- src/dataflow_analyses/dvad_plugin.ml
+++ src/dataflow_analyses/dvad_plugin.ml
@@ -1,8 +1,63 @@
 open Javalib_pack
 open Sawja_pack
 
-(** Fill debug information, depending if dead
-    affectations are detected*)
+
+let colors = ["red";"green";"blue";"orange";"grey"
+	     ;"darkred";"darkgreen";"darkblue";"goldenrod"
+	     ;"black";"indigo";"sienna";"seagreen";"crimson"]
+let modulo = List.length colors
+
+
+let fill_debug_infos dead_found cn ms code live plugin_infos =
+  let _infos = 
+    if dead_found
+    then
+      begin
+	(* We give information of the variables liveness for this
+	   method*)
+	Array.iteri
+	  (fun i _ -> 
+	     let info_live = 
+	       let html_rep live = 
+		 let cpt = ref 0 in
+		   snd 
+		     (Live_bir.Env.fold 
+			(fun var (pre,html) -> 
+			   incr cpt;
+			   (", ",html^pre
+			      ^"<span style=\"color:"
+			      ^(List.nth colors ((!cpt-1) mod modulo))^";\">"
+			      ^JBir.var_name_g var^"</span>"
+			   ))
+			live
+			("",""))
+	       in
+		 Printf.sprintf ("<div><b>LIVE[%s]</b> = {%s}</div>")
+		   (string_of_int i)
+		   (html_rep (live i))
+	     in
+	       plugin_infos.JPrintPlugin.p_infos <- 
+		 JPrintPlugin.add_pp_iow info_live cn ms i plugin_infos.JPrintPlugin.p_infos
+	  )
+	  code.JBir.code;
+	plugin_infos.JPrintPlugin.p_infos <- 
+	  JPrintPlugin.add_meth_iow 
+	  (JPrintPlugin.MethodSignature "Dead variable affectation(s) found") 
+	  cn ms plugin_infos.JPrintPlugin.p_infos ;
+      end
+    else
+      plugin_infos.JPrintPlugin.p_infos <- 
+	JPrintPlugin.add_meth_iow 
+	(JPrintPlugin.MethodSignature "No dead variable affectation found") 
+	cn ms plugin_infos.JPrintPlugin.p_infos;
+  in ()
+
+
+
+(** Fill debug information, depending if dead affectations are
+    detected*)
+(*
+
 let fill_debug_infos dead_found cn ms code live plugin_infos =
   let _warns = 
     if dead_found
@@ -15,12 +70,13 @@ let fill_debug_infos dead_found cn ms code live plugin_infos =
     if dead_found
     then
       begin
-      (* We give information of the variables liveness for this
-	 method*)
+	(* We give information of the variables liveness for this
+	   method*)
       Array.iteri
 	(fun i _ -> 
 	   let info_live = 
-	     Printf.sprintf "Live variables: %s\n"
+	     Printf.sprintf "LIVE[%s] = %s\n"
+	       (string_of_int i)
 	       (Live_bir.to_string (live i)) 
 	   in
 	     plugin_infos.JPrintPlugin.p_infos <- 
@@ -37,8 +93,11 @@ let fill_debug_infos dead_found cn ms code live plugin_infos =
 	JPrintPlugin.add_meth_iow 
 	  (JPrintPlugin.MethodSignature "No dead variable affectation found") 
 	  cn ms plugin_infos.JPrintPlugin.p_infos;
-  in ()
-    
+  in ()*)
+     
+
+
+
 (**[method_dead_affect cn ms code live plug_info ] modifies the data
    structure [plug_info] containing information for the Eclipse
    plugin. [cn] is a class_name, [ms] a method_signature, [code] the
@@ -85,13 +144,17 @@ let method_dead_affect cn ms code live plugin_infos =
       code.JBir.code;
     (* Fill information for plugin depending on the method analysis result*)
     fill_debug_infos !dead_var_exists cn ms code live plugin_infos
-		    
+	    
       
+
+
+
 (**[method_dead_affect ioc] returns the data structure containing
    information for the Eclipse plugin. [ioc] is the interface_or_class
    to check.*)
 let run_dead_affect ioc =
-  let plugin_infos = JPrintPlugin.JBirPrinter.empty_infos in
+ 
+ let plugin_infos = JPrintPlugin.JBirPrinter.empty_infos in
     Javalib.cm_iter
       (fun cm ->
 	 match cm.Javalib.cm_implementation with 
@@ -103,6 +166,9 @@ let run_dead_affect ioc =
 		 method_dead_affect cn ms code live plugin_infos)
       ioc;
     plugin_infos
+
+
+
 
 (**[main cp output cn_string] loads the class [cn_string], converts it
    in JBir representation, runs the analysis and print the information
@@ -116,7 +182,10 @@ let main cp output cn_string =
   let plugin_infos = run_dead_affect bir_ioc in
     (* Print infos on the current class for the Eclipse plugin*)
     JPrintPlugin.JBirPrinter.print_class plugin_infos bir_ioc output
-      
+
+
+
+
 (** [_plugin_exec] parses arguments of executable and executes the
     analysis for each class file given by the arguments*)
 let _plugin_exec = 
---
+++ src/dataflow_analyses/live_bir.mli
@@ -0,0 +1,66 @@
+(*
+ * This file is part of SAWJA
+ * Copyright (c)2010 David Pichardie (INRIA)
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *)
+
+(** Live variable analysis for {!JBir} representation*)
+
+(** {2 Domain of the live variable analysis} *)
+
+(** A set of live variables*)
+module Env :
+  sig
+    type elt = JBir.var
+    type t
+    val mem : elt -> t -> bool
+    val iter : (elt -> unit) -> t -> unit
+    val fold : (elt -> 'a -> 'a) -> t -> 'a -> 'a
+    val for_all : (elt -> bool) -> t -> bool
+    val exists : (elt -> bool) -> t -> bool
+
+    val empty : t
+    val is_empty : t -> bool
+    val add : elt -> t -> t
+    val singleton : elt -> t
+    val remove : elt -> t -> t
+    val union : t -> t -> t
+    val inter : t -> t -> t
+    val diff : t -> t -> t
+    val compare : t -> t -> int
+    val equal : t -> t -> bool
+    val subset : t -> t -> bool
+    val filter : (elt -> bool) -> t -> t
+    val partition : (elt -> bool) -> t -> t * t
+    val cardinal : t -> int
+    val elements : t -> elt list
+    val min_elt : t -> elt
+    val max_elt : t -> elt
+    val choose : t -> elt
+    val split : elt -> t -> t * bool * t
+    val to_string : t -> string
+  end
+
+(** [to_string env] is a printer for the analysis domain.*)
+val to_string : Env.t -> string
+
+(** {2 Launch the analysis}*)
+
+(** [run code] returns a function [f pc] that returns the set of live
+variables before execution of the program point [pc]*)
+val run : JBir.t -> int -> Env.t
+
+
