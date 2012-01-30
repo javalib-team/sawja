62090fae881820bc057cfae3dddc398bebb57860
David Pichardie <david.pichardie@irisa.fr>
2012-01-30 19:20:57 +0000

    this runtime code is necessary to use the new Formula instruction of each IR

---
+++ runtime/sawja/Assertions.java
@@ -0,0 +1,11 @@
+package sawja;
+
+public class Assertions {
+
+    static public void assume(boolean b) {}
+
+    static public void check(boolean b) {}
+
+    static public void invariant(boolean b) {}
+
+}
